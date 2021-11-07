ffba75d56df767870b093d27abb845eb67cc8b58
alaagoutali815 <alaagoutali91@gmail.com>
2021-11-07 16:20:42 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    AdminComponent,
+    AuthentificationComponent,
+ 
+  
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
